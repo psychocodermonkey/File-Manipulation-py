334666c4e3ac6901d6beae2752c2c63e7a642ee1
Andrew Dixon <PsychoCoderMonkey@gmail.com>
2023-12-15 16:46:18 -0600

    Clean up online commit.
    Add header to file and port code to normal new file template.

--- panagramCheck.py
+++ panagramCheck.py
@@ -1,3 +1,12 @@
+#! /usr/bin/env python3
+'''
+# Program: Script checking if strings are a panagram.
+#    Name: Andrew Dixon            File: panagramCheck.py
+#    Date: 15 Dec 2023
+#   Notes:
+#
+#........1.........2.........3.........4.........5.........6.........7.........8.........9.........0.........1.........2.........3..
+'''
 
 from string import ascii_lowercase
 
@@ -7,14 +16,14 @@ def main() -> None:
         'The five boxing wizards jump quickly.',
         'Jackdaws love my big sphinx of quartz.',
         'Mr. Jock, TV quiz PhD., bags few lynx.',
-        'Pack my box with five dozen liquor jugs.', 
+        'Pack my box with five dozen liquor jugs.',
         'Waxy and quivering jocks fumble the pizza.',
         'The quick brown fox jumps over the lazy dog.',
         'The quick brown fox jumped over the lazy dog.',
         'A wizard\'s job is to vex chumps quickly in fog.',
-        """This Pangram contains four a's, one b, two c's, one d, thirty e's, six f's, 
-            five g's, seven h's, eleven i's, one j, one k, two l's, two m's, eighteen n's, 
-            fifteen o's, two p's, one q, five r's, twenty-seven s's, eighteen t's, two u's, 
+        """This Pangram contains four a's, one b, two c's, one d, thirty e's, six f's,
+            five g's, seven h's, eleven i's, one j, one k, two l's, two m's, eighteen n's,
+            fifteen o's, two p's, one q, five r's, twenty-seven s's, eighteen t's, two u's,
             seven v's, eight w's, two x's, three y's, & one z."""
     )
 
@@ -33,5 +42,7 @@ def check_string(my_string):
         value = f'The string "{my_string}" is missing the following letters: {diff}'
     return value
 
-if __name__ == "__main__":
-    main()
+# If the panagramCheck.py is run (instead of imported as a module),
+# call the main() function:
+if __name__ == '__main__':
+  main()
